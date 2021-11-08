7f8ccca0a5e380444fec30e164433b1b742a0ce7
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2021-11-08 11:55:41 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,8 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
+
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
