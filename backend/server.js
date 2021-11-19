29768732ab22c428ff355a07342514843923af1b
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2021-11-19 16:25:27 +0530

    server changes

--- backend/server.js
+++ backend/server.js
@@ -12,17 +12,45 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/pathkoli', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-}
-)
-.then(
-console.log("Connected to Database")
-)
+/*mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/pathkoli', {
+ /* useNewUrlParser: true,
+ /* useUnifiedTopology: true,
+ /* useCreateIndex: true,
+/*}
+/* )
 
-.catch(err=>console.err);
+/*.then(
+/*console.log("Connected to Database")
+/*)
+
+/*.catch(err=>console.err);*/
+
+const { MongoClient } = require("mongodb");
+
+// Connection URI
+const uri =
+"mongodb+srv://pathkoli:<pathkoli>@cluster0.rnqo4.mongodb.net/pathkoli?retryWrites=true&w=majority"
+
+  //"mongodb+srv://sample-hostname:27017/?maxPoolSize=20&w=majority";
+
+// Create a new MongoClient
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.use('/api/users', userRouter);
