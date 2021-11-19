141dc8193953169ee1384871a62ee820606bc292
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2021-11-19 17:04:43 +0530

    roll back everything

--- backend/server.js
+++ backend/server.js
@@ -12,72 +12,33 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-/*mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/pathkoli', {
- /* useNewUrlParser: true,
- /* useUnifiedTopology: true,
- /* useCreateIndex: true,
-/*}
-/* )
-
-/*.then(
-/*console.log("Connected to Database")
-/*)
-
-/*.catch(err=>console.err);*/
-
-const { MongoClient } = require("mongodb");
-
-// Connection URI
-const uri =
-"mongodb+srv://pathkoli:<pathkoli>@cluster0.rnqo4.mongodb.net/pathkoli?retryWrites=true&w=majority"
-
-  //"mongodb+srv://sample-hostname:27017/?maxPoolSize=20&w=majority";
-
-// Create a new MongoClient
-const client = new MongoClient(uri);
-
-async function run() {
-  try {
-    // Connect the client to the server
-    await client.connect();
-
-    // Establish and verify connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Connected successfully to server");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
-app.use('/api/orders',orderRouter);
-app.get('/api/config/paypal',(req,res)=>{
-  res.send(process.env.PAYPAL_CLIENT_ID||'sb'); //sb=sandbox
+app.use('/api/orders', orderRouter);
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');  // sb =sandbox
 });
-
 const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
 
-
-
-/* app.get('/', (req, res) => {
-   res.send('Server is ready');
- });*/
-
-
- app.use((err, req, res, next) => {
+app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5004;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
